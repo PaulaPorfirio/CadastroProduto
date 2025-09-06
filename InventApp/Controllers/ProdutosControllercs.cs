617bac721ed007ad375de797a3a52ae7542d8c62
Paula <paulaporfirio076@gmail.com>
2025-09-06 09:24:27 -0300

    02 - Adicionado Model "Produtos" e configurações iniciais para o Controller

---
+++ InventApp/Controllers/ProdutosControllercs.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using InventApp.Models;
+
+namespace InventApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosControllercs : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok(new { status = "ok" });
+    }
+}
---
+++ InventApp/Models/Produtos.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static InventApp.Models.Produtos;
+
+namespace InventApp.Models
+{
+    public class Produtos 
+    {
+        public int Codigo { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Categoria { get; set; }
+
+        [Required]
+        public decimal ValorUnitario {  get; set; }
+
+        [Required]
+        public decimal Quantidade {  get; set; }
+
+        [Required, DataType(DataType.DateTime)]
+        public DateTime DataCadastro {  get; set; } = DateTime.UtcNow;
+
+        public bool Disponivel { get; } 
+
+    }
+}
